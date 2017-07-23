3bf5bcfa193c1a42a39e247c82cb457c012ee97f
rawtoast <jim@jamesmorris.org>
2017-07-23 22:06:19 +0100

    Builders exercises

--- src/v_builders/_37_StringAndMapBuilders.kt
+++ src/v_builders/_37_StringAndMapBuilders.kt
@@ -1,7 +1,6 @@
 package v_builders
 
 import util.TODO
-import java.util.*
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -19,6 +18,12 @@ fun buildStringExample(): String {
     }
 }
 
+fun buildMap(build: MutableMap<Int, String>.() -> Unit): MutableMap<Int, String> {
+    val map: MutableMap<Int, String> = emptyMap<Int, String>().toMutableMap()
+    build.invoke(map)
+    return map
+}
+
 fun todoTask37(): Nothing = TODO(
     """
         Task 37.
@@ -29,11 +34,11 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+//    todoTask37()
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
--- src/v_builders/_39_HtmlBuilders.kt
+++ src/v_builders/_39_HtmlBuilders.kt
@@ -1,40 +1,52 @@
 package v_builders
 
+import ii_collections.whatFoldDoes
 import util.TODO
 import util.doc39
 import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(row: Int, column: Int) = if ((row + column) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(row: Int, column: Int) = if ((row + column) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun todoTask39(): Nothing = TODO(
-    """
+        """
         Task 39.
         1) Fill the table with the proper values from products.
         2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
         Pass a color as an argument to functions 'tr', 'td'.
         You can call the 'main' function in the 'htmlDemo.kt' to see the rendered table.
     """,
-    documentation = doc39()
+        documentation = doc39()
 )
 
 fun renderProductTable(): String {
     return html {
         table {
             tr {
-                td {
+                td(color = getTitleColor()) {
                     text("Product")
+
                 }
-                td {
+                td(color = getTitleColor()) {
                     text("Price")
                 }
-                td {
+                td(color = getTitleColor()) {
                     text("Popularity")
                 }
             }
             val products = getProducts()
-            todoTask39()
+
+            var c = 1
+
+            for (p in products) {
+                tr {
+                    td(color = getCellColor(c,1)) { text(p.description) }
+                    td(color = getCellColor(c,2)) { text(p.price) }
+                    td(color = getCellColor(c,3)) { text(p.popularity) }
+                }
+                c =+1
+            }
         }
     }.toString()
 }
